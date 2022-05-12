14bbca2272225a321466b44c1ef10582dce0f0f6
Craig <indigomx9@gmail.com>
2022-05-12 16:40:39 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# CRUD Toolkit
+
+Initial setup of the project.  Created with TypeScript, React, Vite, Redux-Toolkit, and Router 6.
+
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis voluptatibus, ipsum recusandae sit aspernatur excepturi nisi deserunt, ut maiores numquam explicabo consequatur corporis mollitia esse amet incidunt veniam, quibusdam itaque.</p>
+        </React.Fragment>
+    );
+};
+
+
