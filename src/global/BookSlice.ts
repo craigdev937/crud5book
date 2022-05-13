37c64dd725336d45c38313ebc0708b9dbe0cc0cc
Craig <indigomx9@gmail.com>
2022-05-12 21:30:06 -0400

    Updated FetchAPI and BookSlice with all CRUD methods.

--- src/global/BookSlice.ts
+++ src/global/BookSlice.ts
@@ -25,6 +25,60 @@ const BookSlice = createSlice({
             state.loading = false,
             state.books = [...action.payload]
         },
+        [API.getOne.rejected.type]: (state, action) => {
+            state.loading = false,
+            state.error = action.payload
+        },
+        [API.getOne.pending.toString()]: (state) => {
+            state.loading = true
+        },
+        [API.getOne.fulfilled.type]:
+        (state, action: PayloadAction<IBook>) => {
+            state.loading = false,
+            state.books = [action.payload]
+        },
+        [API.createBook.rejected.type]: (state, action) => {
+            state.loading = false,
+            state.error = action.payload
+        },
+        [API.createBook.pending.toString()]: (state) => {
+            state.loading = true
+        },
+        [API.createBook.fulfilled.type]:
+        (state, action: PayloadAction<IBook>) => {
+            state.loading = false,
+            state.books = [action.payload];
+        },
+        [API.updateBook.rejected.type]: (state, action) => {
+            state.loading = false,
+            state.error = action.payload
+        },
+        [API.updateBook.pending.toString()]: (state) => {
+            state.loading = true
+        },
+        [API.updateBook.fulfilled.type]: 
+        (state, action: PayloadAction<IBook>) => {
+            state.loading = false;
+            const index = state.books.findIndex(
+                (book) => book._id === action.payload._id);
+            state.books[index] = {
+                ...state.books[index],
+                ...action.payload
+            };
+        },
+        [API.deleteBook.rejected.type]: (state, action) => {
+            state.loading = false,
+            state.error = action.payload
+        },
+        [API.deleteBook.pending.toString()]: (state) => {
+            state.loading = true
+        },
+        [API.deleteBook.fulfilled.type]:
+        (state, action: PayloadAction<IBook>) => {
+            state.loading = false,
+            state.books.filter(
+                (book) => book._id !== action.payload._id);
+        },
     }
 });
 
--- src/global/FetchAPI.ts
+++ src/global/FetchAPI.ts
@@ -11,6 +11,60 @@ class FetchClass {
         const books: IBook[] = await res.json();
         return [...books];
     });
+
+    getOne = createAsyncThunk("book/getOne", 
+    async ({_id}: IBook) => {
+        const res: Response = 
+        await fetch(`${URL}/${_id}`);
+        if (!res.ok) throw new Error(res.statusText);
+        const book: IBook = await res.json();
+        return book;
+    });
+
+    createBook = createAsyncThunk("book/createBook", 
+    async (payload: IBook) => {
+        const res: Response =
+        await fetch(URL, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                title: payload.title, first: payload.first,
+                last: payload.last, age: payload.age,
+                info: payload.info
+            }),
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        const book: IBook = await res.json();
+        return book;
+    });
+
+    updateBook = createAsyncThunk("book/updateBook", 
+    async (payload: IBook) => {
+        const res: Response = 
+        await fetch(`${URL}/${payload._id}`, {
+            method: "PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                title: payload.title, first: payload.first,
+                last: payload.last, age: payload.age,
+                info: payload.info
+            }),
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        const book: IBook = await res.json();
+        return book;
+    });
+
+    deleteBook = createAsyncThunk("book/deleteBook", 
+    async ({_id}: IBook) => {
+        const res: Response = 
+        await fetch(`${URL}/${_id}`, {
+            method: "DELETE"
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        const book: IBook = await res.json();
+        return book;
+    });
 };
 
 export const API: FetchClass = new FetchClass();
